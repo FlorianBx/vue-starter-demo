e27da55968da154fc1681ffe14d053e2acffd5bf
Florian <25250981+BFlorian91@users.noreply.github.com>
2024-11-02 11:31:20 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <h1 class="text-6xl text-red-400 font-bold font-mono">Hello, World!</h1>
+</template>
