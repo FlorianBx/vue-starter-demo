c7e8e26d35452c1adfaf69e0c7f682fa3c6e08da
Florian <25250981+BFlorian91@users.noreply.github.com>
2024-11-02 12:29:31 +0100

    feat: add basic router

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,23 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <h1 class="text-6xl text-red-400 font-bold font-mono">Hello, World!</h1>
+  <nav class="flex gap-4 justify-end mr-4 p-4">
+    <RouterLink
+      class="bg-blue-400 hover:bg-blue-600 rounded w-24 flex items-center justify-center py-1.5 px-4 text-white"
+      to="/"
+      >Home</RouterLink
+    >
+    <RouterLink
+      class="bg-blue-400 hover:bg-blue-600 rounded w-24 flex items-center justify-center py-1.5 px-4 text-white"
+      to="/about"
+      >About</RouterLink
+    >
+    <RouterLink
+      class="bg-blue-400 hover:bg-blue-600 rounded w-24 flex items-center justify-center py-1.5 px-4 text-white"
+      to="/user/21"
+      >User 21</RouterLink
+    >
+  </nav>
+
+  <RouterView />
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import UserView from '../views/UserView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', component: HomeView },
+    { path: '/about', component: AboutView },
+    { path: '/user/:id', component: UserView },
+  ],
 })
 
 export default router
